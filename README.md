0e817e0893dbc59b13ec8f61a0f4a1b4a2568117
elf Pavlik <perpetual-tripper@wwelves.org>
2013-09-18 16:39:22 +0200

    accepting and serving files

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # DSpace Media Server
 
-Server accepting file uploads, serving them and messaging using Bayeux
-protocol
+Server accepting file uploads and serving them
+
+    $ npm install
+    $ cp config.json.example config.json
+    $ node server.js
+
+expects uploads to have meta parameter with JSON object { noteUUID: "" } used for filename
---
+++ server.js
@@ -0,0 +1,47 @@
+var nconf = require('nconf');
+var express = require('express');
+var cors = require('cors');
+var fs = require('fs');
+
+/*
+ * get config from file
+ */
+nconf.file({ file: 'config.json' });
+
+
+/*
+ * static media files
+ */
+
+var app = express();
+
+// enable CORS
+app.use(cors());
+
+// serve static files
+app.use(express.static(nconf.get('uploads').path));
+
+app.use(express.multipart());
+
+function saveAttachement(fields, files) {
+  var noteUUID = JSON.parse(fields.meta).noteUUID;
+  var destination = nconf.get('uploads').path + '/' +  noteUUID;
+
+  var is = fs.createReadStream(files.file.path);
+  var os = fs.createWriteStream(destination);
+
+  is.pipe(os);
+  is.on('end',function() {
+    fs.unlinkSync(files.file.path);
+  });
+}
+
+app.post('/', function(req, res) {
+  res.writeHead(200);
+  res.end(saveAttachement(req.body, req.files));
+});
+
+var port = nconf.get('server').port;
+app.listen(port, function() {
+  console.log('listening on ' + port);
+});
